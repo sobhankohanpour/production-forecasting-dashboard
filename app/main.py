bd4eb889eb5e4c08041dfd5ae04cd051b9c8547e
Sobhan Kohanpour <kohanpour.sobhan@gmail.com>
2025-11-20 21:03:45 +0330

    update: '📁 Select/Upload Dataset' tab now supports user file uploads

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+import os
+import pandas as pd
 import streamlit as st
 
+save_dir = os.path.join("data") 
+
+# Ensure the folder exists
+os.makedirs(save_dir, exist_ok=True)
 
 home, upload_dataset, preprocessing_tab, train_ml_model = st.tabs([
     "🏠 Home", 
@@ -28,9 +34,29 @@ with home:
         "visualizations, and AI-powered predictions designed for real-world petroleum engineering workflows."
         )
 with upload_dataset:
-    uploaded_file = st.file_uploader("Upload your dataset", type=["csv", "xlsx"])
-    uploaded_file
+    if st.toggle("I want to upload my dataset."):
+        number_of_user_dataset = st.slider(
+            "Number of dataset(s)", min_value=1, max_value=50, value=1
+        )
+        st.write(f"You must upload {number_of_user_dataset} dataset(s).")
+
+        uploaded_files = []
+        for i in range(number_of_user_dataset):
+            uploaded_file = st.file_uploader(
+                f"Upload dataset #{i+1}", type=["xlsx"], key=f"dataset_{i}"
+            )
+            if uploaded_file:
+                uploaded_file.seek(0)  # reset file pointer
+                df_uploaded_file = pd.read_excel(uploaded_file)
+                st.dataframe(df_uploaded_file)
+
+                # Save with original name
+                save_path = os.path.join(save_dir, uploaded_file.name)
+                df_uploaded_file.to_excel(save_path, index=False)
+                st.success(f"Dataset #{i+1} saved.")
+
 with preprocessing_tab:
     st.write("Data preprocessing...")
+
 with train_ml_model:
     st.write("Training the ML model...")
